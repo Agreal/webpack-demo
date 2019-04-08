25d18e79943c16c83d40238221c77e1808af4f1d
Qin Xiaoqiang <qxq111@gmail.com>
2019-04-08 21:59:02 +0800

    finish capture 2

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg)/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml/,
+                use: ['xml-loader']
+            }
+        ]
     }
 };
