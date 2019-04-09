abf96422f04e70fc76b37c9d8eee8f6f37017cce
Qin Xiaoqiang <qxq111@gmail.com>
2019-04-09 19:12:02 +0800

    done Development

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    // devtool: 'inline-source-map',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'output management'
         })
-    ]
+    ],
+    devServer: {
+        contentBase: './dist'
+    }
 };
