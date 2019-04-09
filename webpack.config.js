a66548996b937808eaf6e1b17a140d33f25c0b4a
Qin Xiaoqiang <qxq111@gmail.com>
2019-04-09 20:18:51 +0800

    to build vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     mode: 'development',
     // devtool: 'inline-source-map',
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        main: './src/main.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'output management'
-        })
+        new HtmlWebpackPlugin(),
+        new VueLoaderPlugin()
     ],
     devServer: {
         contentBase: './dist'
