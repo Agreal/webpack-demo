03a1c8dfceb4d7ffec68645d1da3f62c7079d031
Qin Xiaoqiang <qxq111@gmail.com>
2019-04-12 22:22:53 +0800

    style: formatting code

--- webpack.config.js
+++ webpack.config.js
@@ -5,55 +5,55 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    // devtool: 'inline-source-map',
-    entry: {
-        main: './src/main.js'
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.png$/,
-                use: [
-                    'file-loader'
-                ]
-            }
+  mode: 'development',
+  // devtool: 'inline-source-map',
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            templateParameters: {
-                BASE_URL: ''
-            }
-        }),
-        new CopyPlugin([
-            { from: 'public/favicon.ico', to: '.' },
-        ]),
-        new VueLoaderPlugin()
-    ],
-    devServer: {
-        contentBase: './dist'
-    }
+      },
+      {
+        test: /\.png$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      templateParameters: {
+        BASE_URL: ''
+      }
+    }),
+    new CopyPlugin([
+      {from: 'public/favicon.ico', to: '.'},
+    ]),
+    new VueLoaderPlugin()
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
 };
